ade182230a878e4768c528ce3ba280be9bf883e8
Fabian Weik <fabian.weik@protonmail.com>
2024-08-25 15:47:39 +0200

    get all customers of user

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,51 @@
+use anyhow::Error;
 use reqwest::Client;
 use rusttwald::apis::{
     configuration::{ApiKey, Configuration},
-    customer_api::customer_get_customer,
-    project_api::{project_list_projects, project_list_servers},
+    customer_api::customer_list_customers,
 };
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     let api_token = env!("MITTWALD_API_TOKEN");
 
     let client = Client::new();
 
     let config = Configuration {
-        base_path: "https://api.mittwald.de/v2/".to_string(),
+        base_path: "https://api.mittwald.de".to_string(),
         user_agent: Some("rusttwald - Unofficial Rust API Client".to_string()),
         client,
         basic_auth: None,
         oauth_access_token: None,
         bearer_access_token: None,
-        api_key: Some(ApiKey{
+        api_key: Some(ApiKey {
             prefix: None,
             key: api_token.to_string(),
         }),
     };
 
-    let result = customer_get_customer(&config, "self").await;
-
     //project_list_servers(&config, customer_id, limit, page, skip)
 
     //project_list_projects(&config, customer_id, server_id, None, None, None);
 
-    println!("Hello, world!\n{:?}", result);
+    let customers = get_customers(&config).await?;
+
+    println!("{:?}", customers);
+
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+pub struct Customer {
+    pub id: String,
+}
+
+pub async fn get_customers(config: &Configuration) -> Result<Vec<Customer>, Error> {
+    let customer_list = customer_list_customers(config, None, None, None, None, None).await?;
+    Ok(customer_list
+        .iter()
+        .map(|customer| Customer {
+            id: customer.customer_id.to_string(),
+        })
+        .collect())
 }
